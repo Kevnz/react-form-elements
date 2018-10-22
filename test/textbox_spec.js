dec8b6a14381f3fd2849fcf545a94330a7fc901b
Kevin Isom <kevin.isom@gmail.com>
2018-10-23 07:48:44 +1300

    first update

--- test/textbox_spec.js
+++ test/textbox_spec.js
@@ -1,29 +1,24 @@
-import React from 'react/addons';
-import assert from 'assert';
-import TextBox from '../src/textbox';
-let TestUtils = React.addons.TestUtils;
+import assert from 'assert'
+import React from 'react/addons'
+import TextBox from '../src/textbox'
+let TestUtils = React.addons.TestUtils
 
+describe('TextBox component', () => {
+  describe('The Rendered HTML', () => {
+    var component, textbox
+    before('render and locate textbox element', () => {
+      console.log('render into document')
+      console.log(self)
+      this.component = TestUtils.renderIntoDocument(<TextBox />)
+      this.texbox = TestUtils.findRenderedDOMComponentWithClass(
+        component,
+        'form-row_container'
+      )
+      console.log(textbox)
+    })
 
-        
-describe('TextBox component',() => {
-    describe('The Rendered HTML', () => {
-        var component, textbox;
-        before('render and locate textbox element', () => {
-            console.log('render into document');
-            console.log(self);
-            this.component = TestUtils.renderIntoDocument(<TextBox  />); 
-            this.texbox = TestUtils.findRenderedDOMComponentWithClass(
-                component,
-                'form-row_container'
-            );
-            console.log(textbox)
-        });
-    
-        it('should have a label', () => {  
-            assert(self.textbox.getDOMNode().querySelector('label') );
-        });
-        
-    });
-
-
-});
+    it('should have a label', () => {
+      assert(self.textbox.getDOMNode().querySelector('label'))
+    })
+  })
+})
