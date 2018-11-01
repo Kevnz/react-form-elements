5162550381e91d553c27574d700ea450102793ec
Kevin Isom <kevin.isom@gmail.com>
2018-11-01 22:00:08 +1300

    exports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 'use strict'
 
+import Radio from './radio'
+import Date from './date'
 import TextBox from './textbox'
 import TextArea from './textarea'
 import DropDown from './dropdown'
-import CheckBox from './checkbox'
+import Checkbox from './checkbox'
 import Telephone from './telephone'
 import Range from './range'
 
-export { TextBox }
-export { TextArea }
-export { DropDown }
-export { CheckBox }
-export { Telephone }
-export { Range }
+export {
+  TextBox,
+  TextArea,
+  DropDown,
+  Checkbox,
+  Telephone,
+  Range,
+  Radio,
+  Date,
+  Checkbox as CheckBox,
+}
