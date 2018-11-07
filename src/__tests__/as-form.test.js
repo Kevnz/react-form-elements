118fe00c6f47f23f5fe18c18fa0c21fc9296b569
Kevin Isom <kevin.isom@gmail.com>
2018-11-08 07:51:24 +1300

    Update form test

--- src/__tests__/as-form.test.js
+++ src/__tests__/as-form.test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { render, fireEvent, cleanup } from 'react-testing-library'
 
 import {
@@ -12,21 +12,27 @@ import {
   OptionGroup,
 } from '../index'
 
-class App extends React.Component {
-  constructor(props) {
-    const references = {
-      textBox: React.createRef(),
-      checkBox: React.createRef(),
-      range: React.createRef(),
-      date: React.createRef(),
-      telephone: React.createRef(),
-      dropdown: React.createRef(),
-    }
-    super(props)
-    this.references = references
+const Form = ({ name, onSubmit, children }) => {
+  return (
+    <form name={name} onSubmit={onSubmit}>
+      {' '}
+      {children}
+    </form>
+  )
+}
+
+const App = () => {
+  const references = {
+    textBox: useRef(null),
+    checkBox: useRef(null),
+    range: useRef(null),
+    date: useRef(null),
+    telephone: useRef(null),
+    dropdown: useRef(null),
   }
-  getFormValues() {
-    if (this.references.textBox.current === null)
+
+  const getFormValues = () => {
+    if (references.textBox.current === null)
       return {
         textBox: '',
         checkBox: '',
@@ -36,83 +42,91 @@ class App extends React.Component {
         dropdown: '',
       }
     return {
-      textBox: this.references.textBox.current.getValue(),
-      checkBox: this.references.checkBox.current.getValue(),
-      range: this.references.range.current.getValue(),
-      date: this.references.date.current.getValue(),
-      telephone: this.references.telephone.current.getValue(),
-      dropdown: this.references.dropdown.current.getValue(),
+      textBox: references.textBox.current.getValue(),
+      checkBox: references.checkBox.current.getValue(),
+      range: references.range.current.getValue(),
+      date: references.date.current.getValue(),
+      telephone: references.telephone.current.getValue(),
+      dropdown: references.dropdown.current.getValue(),
     }
   }
+  let formValues = getFormValues()
 
-  render() {
-    const formValues = this.getFormValues()
-    console.log('render')
-    return (
-      <div>
-        <h1>React Form Elements</h1>
-        <form>
-          <TextBox label="My Label" ref={this.references.textBox} />
-          <Checkbox
-            label="My Checkbox"
-            ref={this.references.checkBox}
-            initialValue={true}
-          />
-          <DateTime label="My Date" ref={this.references.date} />
-          <Range label="My Range" ref={this.references.range} />
-          <Telephone label="Telephone" ref={this.references.telephone} />
-          <DateTime label="My DateTime" type="datetime" />
-          <DateTime label="My Month" type="month" />
-          <DateTime label="My Week" type="week" />
-          <DateTime label="My DateTime" type="datetime-local" />
-          <DateTime label="My Time" type="time" />
-          <DropDown
-            label="My Drop Down"
-            initialValue="2"
-            data-testid="dd1"
-            ref={this.references.dropdown}
-          >
-            <OptionGroup label="First Group">
-              <Option initialValue="1">First</Option>
-              <Option initialValue="2">Second</Option>
-              <Option initialValue="3" label="Third" />
-            </OptionGroup>
-            <OptionGroup label="Second Group">
-              <Option initialValue="11">Second First</Option>
-              <Option initialValue="12">Second Second</Option>
-              <Option initialValue="13" label="Second Third" />
-            </OptionGroup>
-            <OptionGroup label="Third Group">
-              <Option initialValue="21">Third First</Option>
-              <Option initialValue="22">Third Second</Option>
-              <Option initialValue="23" label="Third Third" />
-            </OptionGroup>
-          </DropDown>
-          <button
-            onClick={e => {
-              e.preventDefault()
-            }}
-          >
-            Check
-          </button>
-        </form>
-        <div data-testid="ref-out">
-          <ul>
-            <li data-testid="textbox">TextBox: {formValues.textBox}</li>
-            <li data-testid="checkbox">Checkbox: {formValues.checkBox}</li>
-            <li data-testid="range">Range: {formValues.range}</li>
-            <li data-testid="date">Date: {formValues.date}</li>
-            <li data-testid="telephone">Telephone: {formValues.telephone}</li>
-          </ul>
-        </div>
+  useEffect(() => {
+    // Update the document title using the browser API
+    formValues = getFormValues()
+  })
+
+  return (
+    <div>
+      <h1>React Form Elements</h1>
+      <Form
+        name="testForm"
+        onSubmit={e => {
+          e.target.preventDefault()
+          console.log('ON_SUBMIT')
+        }}
+      >
+        <TextBox label="My Label" ref={references.textBox} />
+        <Checkbox
+          label="My Checkbox"
+          ref={references.checkBox}
+          initialValue="{true}"
+        />
+        <DateTime label="My Date" ref={references.date} />
+        <Range label="My Range" ref={references.range} />
+        <Telephone label="Telephone" ref={references.telephone} />
+        <DateTime label="My DateTime" type="datetime" />
+        <DateTime label="My Month" type="month" />
+        <DateTime label="My Week" type="week" />
+        <DateTime label="My DateTime" type="datetime-local" />
+        <DateTime label="My Time" type="time" />
+        <DropDown
+          label="My Drop Down"
+          initialValue="2"
+          data-testid="dd1"
+          ref={references.dropdown}
+        >
+          <OptionGroup label="First Group">
+            <Option initialValue="1">First</Option>
+            <Option initialValue="2">Second</Option>
+            <Option initialValue="3" label="Third" />
+          </OptionGroup>
+          <OptionGroup label="Second Group">
+            <Option initialValue="11">Second First</Option>
+            <Option initialValue="12">Second Second</Option>
+            <Option initialValue="13" label="Second Third" />
+          </OptionGroup>
+          <OptionGroup label="Third Group">
+            <Option initialValue="21">Third First</Option>
+            <Option initialValue="22">Third Second</Option>
+            <Option initialValue="23" label="Third Third" />
+          </OptionGroup>
+        </DropDown>
+        <button
+          onClick={e => {
+            e.preventDefault()
+          }}
+        >
+          Check
+        </button>
+      </Form>
+      <div data-testid="ref-out">
+        <ul>
+          <li data-testid="textbox">TextBox: {formValues.textBox}</li>
+          <li data-testid="checkbox">Checkbox: {formValues.checkBox}</li>
+          <li data-testid="range">Range: {formValues.range}</li>
+          <li data-testid="date">Date: {formValues.date}</li>
+          <li data-testid="telephone">Telephone: {formValues.telephone}</li>
+        </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 describe('The form components as a form', () => {
   afterEach(cleanup)
-  it('should render properly', () => {
+  it('should render and change accordingly', () => {
     const { container, getByLabelText, getByTestId, rerender } = render(<App />)
     const textBox = getByLabelText('My Label')
     const dd = getByTestId('dd1')
@@ -124,6 +138,7 @@ describe('The form components as a form', () => {
     const changedDropDownValue = dd.value
     expect(changedDropDownValue).toBe('22')
     rerender(<App />)
+    expect(textBox.value).toBe('Changed Value')
     expect(container.firstChild).toMatchSnapshot()
   })
 })
