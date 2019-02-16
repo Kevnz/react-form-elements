d0877e32bfea1f68abdfe8046185853cc332d81e
Kevin Isom <kevin.isom@gmail.com>
2019-02-17 09:52:56 +1300

    Add fieldset element and tests

---
+++ src/fieldset/index.js
@@ -0,0 +1,63 @@
+import React, {
+  createRef,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * Fieldset Component.
+ *
+ */
+const Fieldset = forwardRef(({ children, legend, ...props }, ref) => {
+  const mapped = children.map(child => {
+    const displayName = child.type ? child.type.displayName : false
+    if (!displayName) return child
+    return Object.assign({}, child, { ref: createRef() })
+  })
+
+  const formElements = mapped.map(formElement => ({
+    name: formElement.props.name,
+    ref: formElement.ref,
+  }))
+
+  const inputRef = useRef()
+  useImperativeHandle(ref, () => ({
+    isFieldset: true,
+    getValues: () => {
+      const values = {}
+      formElements.forEach(el => {
+        if (!el.name) {
+          return
+        }
+        values[el.name] = el.ref.current.getValue()
+      })
+      return values
+    },
+    getValue: () => {
+      return this.getValues()
+    },
+  }))
+
+  return (
+    <fieldset {...props} ref={inputRef}>
+      <legend>{legend}</legend>
+      {mapped}
+    </fieldset>
+  )
+})
+
+Fieldset.displayName = 'ReactFormElements(Fieldset)'
+
+export default Fieldset
+
+Fieldset.propTypes = {
+  name: PropTypes.string,
+  legend: PropTypes.string,
+}
+
+Fieldset.defaultProps = {
+  name: 'Fieldset',
+  legend: 'legend',
+}
