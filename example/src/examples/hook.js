c6666344cf4cc29983c2e5abf9550c4043c8c7e7
Kevin Isom <kevin.isom@gmail.com>
2019-02-17 13:42:33 +1300

    hook example

---
+++ example/src/examples/hook.js
@@ -0,0 +1,82 @@
+import React, { useState, createRef } from 'react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { tomorrow } from 'react-syntax-highlighter/dist/styles/prism'
+import { Button, Form, useFormElement } from '../../../src'
+
+const ContactForm = () => {
+  const [data, setData] = useState(null)
+  const codeString = `
+  import React, { createRef } from 'react'
+  import {
+    useFormElement,
+    Button,
+    Form,
+  } from 'react-form-elements'
+
+  export default () => {
+    const ref = createRef()
+    const { id, value, handleChange, inputRef } = useFormElement('', ref)
+    return (<Form
+        name="sampleForm"
+        onSubmit={values => {
+          sendData({ formValues: values })
+        }}
+      >
+        <input
+          id={id}
+          name="hookedInput"
+          type="text"
+          value={value}
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <Button>Submit</Button>
+      </Form>)
+    }
+  `
+  const ref = createRef()
+  const { id, value, handleChange, inputRef } = useFormElement('', ref)
+  return (
+    <main>
+      {data && (
+        <section>
+          <h2 className="mar-b-md mar-t-md">Submitted Data</h2>
+          <ul>
+            <li>Hook: {data.formValues.hookedInput}</li>
+          </ul>
+        </section>
+      )}
+      <section>
+        <h2 className="mar-b-md mar-t-md">Hook</h2>
+        <div>
+          <Form
+            name="contactForm"
+            onSubmit={values => {
+              console.log('From Hooked Input', values.hookedInput)
+              setData({ formValues: values })
+            }}
+          >
+            <input
+              id={id}
+              name="hookedInput"
+              type="text"
+              value={value}
+              onChange={handleChange}
+              ref={inputRef}
+            />
+
+            <Button>Submit</Button>
+          </Form>
+        </div>
+        <div className="mar-b-md mar-t-md">
+          <h3 className="mar-b-md">Code from Example</h3>
+          <SyntaxHighlighter language="jsx" style={tomorrow}>
+            {codeString}
+          </SyntaxHighlighter>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default ContactForm
--- example/src/routes.js
+++ example/src/routes.js
@@ -8,6 +8,7 @@ const AttributesForm = React.lazy(() => import('./examples/attributes-form'))
 const SelectorsForm = React.lazy(() => import('./examples/selectors'))
 const Others = React.lazy(() => import('./examples/others'))
 const Styling = React.lazy(() => import('./examples/styling'))
+const Hooks = React.lazy(() => import('./examples/hook'))
 export default () => {
   return (
     <Router>
@@ -18,6 +19,7 @@ export default () => {
       <AttributesForm path="react-form-elements/attributes" />
       <SelectorsForm path="react-form-elements/selectors" />
       <Others path="react-form-elements/others" />
+      <Hooks path="react-form-elements/hook" />
     </Router>
   )
 }
