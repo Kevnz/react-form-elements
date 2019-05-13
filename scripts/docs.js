d6aaacde544232df0e8fcb088a7e662cf00b45ae
Kevin Isom <kevin.isom@gmail.com>
2019-05-14 07:59:53 +1200

    Start work on generating more docs

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,3 +1,12 @@
 const jsdoc2md = require('jsdoc-to-markdown')
+const showdown = require('showdown')
+const showdownHighlight = require('showdown-highlight')
 
-jsdoc2md.render({ files: 'src/*/*.js' }).then(console.log)
+const converter = new showdown.Converter({
+  extensions: [showdownHighlight],
+})
+
+jsdoc2md.render({ files: 'src/*/*.js' }).then(docage => {
+  console.log(docage)
+  console.log(converter.makeHtml(docage))
+})
