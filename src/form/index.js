14f50805567c81fadc2dc5c20e82d6309e2de130
Kevin Isom <kevin.isom@gmail.com>
2019-06-18 22:50:35 +1200

    Flatten objects

--- src/form/index.js
+++ src/form/index.js
@@ -50,8 +50,23 @@ const Form = ({ name, onSubmit, children }) => {
               : el.ref.current.value
           }
         })
+
         e.preventDefault()
-        onSubmit(values)
+        const flattenObject = obj => {
+          const flattened = {}
+
+          Object.keys(obj).forEach(key => {
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+              Object.assign(flattened, flattenObject(obj[key]))
+            } else {
+              flattened[key] = obj[key]
+            }
+          })
+
+          return flattened
+        }
+
+        onSubmit(flattenObject(values))
       }}
     >
       {mapped}
