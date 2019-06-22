ac2c393704d13508d03f5405f865b7afc3ea0f73
Kevin Isom <kevin.isom@gmail.com>
2019-06-22 20:03:52 +1200

    update to get ci to ignore

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,10 +1,16 @@
 const ghpages = require('gh-pages')
 
-ghpages.publish('dist', function(err) {
-  if (err) {
-    console.error('Error Publishing', err)
-    process.exit(1)
+ghpages.publish(
+  'dist',
+  {
+    message: 'Publisher generated commit [ci skip]',
+  },
+  function(err) {
+    if (err) {
+      console.error('Error Publishing', err)
+      process.exit(1)
+    }
+    console.log('published')
+    process.exit(0)
   }
-  console.log('published')
-  process.exit(0)
-})
+)
