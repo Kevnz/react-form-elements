3fec3ebe696d2d4ab0fc20b229c58103d24ccb02
Kevin Isom <kevin.isom@gmail.com>
2019-10-18 12:49:48 +1300

    Add submit to ref

--- src/form/index.js
+++ src/form/index.js
@@ -6,6 +6,29 @@ import React, {
 } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+
+const childMapper = child => {
+  const displayName = child.type ? child.type.displayName : false
+  if (!displayName && !child.ref) return child
+  return Object.assign({}, child, {
+    ref: createRef(),
+    displayName: displayName,
+  })
+}
+const elementsMapper = (fEl, index) => {
+  return {
+    name: fEl.props ? fEl.props.name : `${fEl.name}-${index}`,
+    ref: fEl.ref,
+    displayName: fEl.displayName,
+  }
+}
+const resetElements = fe => {
+  if (fe.ref.current && fe.ref.current.reset) {
+    fe.ref.current.reset()
+  }
+}
+const defaultElements = e => (e.value = '')
+
 /**
  * Form Component.
  *
@@ -13,14 +36,7 @@ import classnames from 'classnames'
 
 const Form = forwardRef(({ name, onSubmit, children, className }, ref) => {
   const mapped = Array.isArray(children)
-    ? children.map(child => {
-        const displayName = child.type ? child.type.displayName : false
-        if (!displayName && !child.ref) return child
-        return Object.assign({}, child, {
-          ref: createRef(),
-          displayName: displayName,
-        })
-      })
+    ? children.map(childMapper)
     : [
         Object.assign({}, children, {
           ref: createRef(),
@@ -28,27 +44,67 @@ const Form = forwardRef(({ name, onSubmit, children, className }, ref) => {
         }),
       ]
 
-  const formElements = mapped.map((fEl, index) => {
-    return {
-      name: fEl.props ? fEl.props.name : `${fEl.name}-${index}`,
-      ref: fEl.ref,
-      displayName: fEl.displayName,
-    }
-  })
+  const formElements = mapped.map(elementsMapper)
 
   const formRef = useRef()
+  const formSubmission = e => {
+    e.preventDefault()
+    let values = {}
+    if (formElements.length === 1) {
+      values =
+        formElements[0].ref.current.getValues() ||
+        formElements[0].ref.current.getValue()
+    }
+    formElements.forEach(el => {
+      if (!el.name) {
+        return
+      }
+      if (!el.ref) {
+        return
+      }
+      if (el.ref && (el.ref.current.isFieldset || el.ref.current.isRow)) {
+        Object.assign(values, el.ref.current.getValues())
+      } else {
+        values[el.name] = el.ref.current.getValue
+          ? el.ref.current.getValue()
+          : el.ref.current.value
+      }
+    })
 
+    e.preventDefault()
+    const flattenObject = obj => {
+      const flattened = {}
+
+      Object.keys(obj).forEach(key => {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+          Object.assign(flattened, flattenObject(obj[key]))
+        } else {
+          flattened[key] = obj[key]
+        }
+      })
+
+      return flattened
+    }
+
+    onSubmit(flattenObject(values))
+  }
   useImperativeHandle(ref, () => ({
     reset: () => {
       formRef.current.reset()
       const ins = formRef.current.querySelectorAll('input')
-      formElements.forEach(fe => {
-        if (fe.ref.current && fe.ref.current.reset) {
-          fe.ref.current.reset()
-        }
+      formElements.forEach(resetElements)
+      const elements = Array.from(ins)
+      elements.forEach(defaultElements)
+    },
+    submit: () => {
+      console.log('form ref', formRef.current)
+      console.log('form ref syb', formRef.current.submit)
+      console.log('synthetic submission')
+      formSubmission({
+        preventDefault: () => {
+          console.log('synthetic submission preventDefault')
+        },
       })
-      const els = Array.from(ins)
-      els.forEach(e => (e.value = ''))
     },
   }))
   return (
@@ -56,47 +112,7 @@ const Form = forwardRef(({ name, onSubmit, children, className }, ref) => {
       ref={formRef}
       name={name}
       className={classnames('rfe-form', className)}
-      onSubmit={e => {
-        e.preventDefault()
-        let values = {}
-        if (formElements.length === 1) {
-          values =
-            formElements[0].ref.current.getValues() ||
-            formElements[0].ref.current.getValue()
-        }
-        formElements.forEach(el => {
-          if (!el.name) {
-            return
-          }
-          if (!el.ref) {
-            return
-          }
-          if (el.ref && (el.ref.current.isFieldset || el.ref.current.isRow)) {
-            Object.assign(values, el.ref.current.getValues())
-          } else {
-            values[el.name] = el.ref.current.getValue
-              ? el.ref.current.getValue()
-              : el.ref.current.value
-          }
-        })
-
-        e.preventDefault()
-        const flattenObject = obj => {
-          const flattened = {}
-
-          Object.keys(obj).forEach(key => {
-            if (typeof obj[key] === 'object' && obj[key] !== null) {
-              Object.assign(flattened, flattenObject(obj[key]))
-            } else {
-              flattened[key] = obj[key]
-            }
-          })
-
-          return flattened
-        }
-
-        onSubmit(flattenObject(values))
-      }}
+      onSubmit={formSubmission}
     >
       {mapped}
     </form>
